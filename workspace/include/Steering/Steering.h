aa8c3edbedd84e256e5be5bcb4878f4714d47bd4
kumicho93 <taniguchi.takumi@gmail.com>
2021-06-22 19:15:35 +0900

    コメントコーディング

--- workspace/include/Steering/Steering.h
+++ workspace/include/Steering/Steering.h
@@ -1,1 +1,17 @@
 
+
+#include "../System/System.h"
+#include "Motor.h"
+
+class Steering
+{
+    private:
+    Steering();
+    ~Steering();
+
+
+
+    public:
+
+    int8 rotateWheel(MotorPower motorPower,Motorport motor_port);
+};
