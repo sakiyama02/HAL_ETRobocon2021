607fe05ab9dbbfab580d7ac6c4b692e3c7d204f7
turuno <godyosihiro@gmail.com>
2021-06-23 17:46:15 +0900

    SceneControl.hのメソッド引数型の変更(init8→init16)

--- workspace/include/Scenario/SceneControl.h
+++ workspace/include/Scenario/SceneControl.h
@@ -11,12 +11,12 @@ class SceneControl{
         ~SceneContorol();
 
         //シーンの切り替え
-        //引数   init8_t シーン番号[IN]
+        //引数   init16_t シーン番号[IN]
         //戻り値 init8_t エラーチェック
-        init8_t SceneChenge(init8_t);
+        init8_t SceneChenge(init16_t);
 
         //実行
-        //引数   init8_t シーン番号[IN]
+        //引数   init16_t シーン番号[IN]
         //戻り値 init8_t エラーチェック
-        init8_t Run(init8_t);
+        init8_t Run(init16_t);
 }
