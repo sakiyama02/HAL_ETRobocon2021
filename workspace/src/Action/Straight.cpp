bf225f5e84deefa512904a06678669710330f389
kumicho93 <taniguchi.takumi@gmail.com>
2021-06-28 18:34:06 +0900

    直進の作成

--- workspace/src/Action/Straight.cpp
+++ workspace/src/Action/Straight.cpp
@@ -1,1 +1,38 @@
 
+#include "../../include/Action/Straight.h"
+
+Straight::Straight(){}
+Straight::~Straight(){}
+
+int8 run(int32 speed,PIDData pid_data = {0.0f,0.0f,0.0f,0.0f},
+    float angle = 0.0f,CurveData curve_data = {0.0f,(CurveType)0}){
+    
+    int8 retChk = SYS_NG;
+    TrapezoidControl &trapezoid = TrapezoidControl::getInstance();
+    Steering &steering          = Steering::getInstance();
+    MotorPower motorPower;
+
+    memset(&motorPower,0,sizeof(MotorPower));
+
+    // 台形制御のターゲット値を設定
+    retChk = trapezoid.setTargetSpeed(speed);
+    if(retChk != SYS_OK){
+        return SYS_NG;
+    }
+    // 台形制御からモータ速度を取得
+    retChk = trapezoid.getMotorPower(&speed);
+        if(retChk != SYS_OK){
+        return SYS_NG;
+    }
+
+    // 計算
+    motorPower.leftPower  = speed;
+    motorPower.rightPower = speed;
+
+    // 計算した値で出力
+    retChk = steering.rotateWheel(motorPower);
+     if(retChk != SYS_OK){
+        return SYS_NG;
+    }
+    return SYS_OK;
+}
