7888883e07d5443fdb99005b9269b5484e57392b
sakiyama <sakiyama.hayato02@gmail.com>
2021-07-02 13:55:40 +0900

    Turn.h cpp and ArmAction.h cpp  add

--- workspace/include/Action/ArmAction.h
+++ workspace/include/Action/ArmAction.h
@@ -1,1 +1,15 @@
 
+#include "../System/System.h"
+#include "Action.h"
+#include "../Steering/Steering.h"
+#include "../Control/TrapezoidControl.h"
+
+class ArmAction : public Action{
+    private:
+    public:
+
+    ArmAction();
+    ~ArmAction();
+
+    int8 run(int32 speed,PIDData pid_data,float angle,CurveData curve_data);
+};
