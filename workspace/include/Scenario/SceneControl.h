b125c973d62647f4c7eea3d6f091adf8360fc421
turuno <godyosihiro@gmail.com>
2021-07-02 18:52:49 +0900

    feature/0005

--- workspace/include/Scenario/SceneControl.h
+++ workspace/include/Scenario/SceneControl.h
@@ -14,7 +14,7 @@ class SceneControl{
         //シーンの切り替え
         //引数   init16_t シーン番号[IN]
         //戻り値 init8_t エラーチェック
-        virtual int8 sceneChenge(&int16)=0;
+        virtual int8 sceneChenge(int16*)=0;
 
         //実行
         //引数   init16_t シーン番号[IN]
