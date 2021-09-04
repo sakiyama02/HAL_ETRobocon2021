f7d36e0a6475fef33f73ceab6ca5006ec0568e2c
HirokaneShota <hirokane.shota@gmail.com>
2021-09-04 18:14:04 +0900

    システムヘッダ追加

--- workspace/include/System/System.h
+++ workspace/include/System/System.h
@@ -11,8 +11,8 @@
 #define SYS_PARAM 2 //引数エラー
 #define TIMEATTACK_NUM 20 
 #define SLALOMEEBUI_NUM 41
-#define SLALOMEIFIE_NUM 19
-#define SLALOMBLACKY_NUM 16
+#define SLALOMEIFIE_NUM 20
+#define SLALOMBLACKY_NUM 17
 #define GARAGE_NUM 25
 
 #define CAR_WIDTH 134.22f      //車体の幅(mm)
