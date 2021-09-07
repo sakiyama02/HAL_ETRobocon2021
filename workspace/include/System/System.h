d590eadf11bfd3dc5627a16360628cd2cf35ce48
HirokaneShota <hirokane.shota@gmail.com>
2021-09-07 19:56:06 +0900

    シーン数変更

--- workspace/include/System/System.h
+++ workspace/include/System/System.h
@@ -13,7 +13,7 @@
 #define SLALOMEEBUI_NUM 21
 #define SLALOMEIFIE_NUM 15
 #define SLALOMBLACKY_NUM 16
-#define GARAGE_NUM 25
+#define GARAGE_NUM 26
 
 #define CAR_WIDTH 134.22f      //車体の幅(mm)
 #define CAR_WHEEL_WIDTH 90.33f //車輪の直径(mm)
