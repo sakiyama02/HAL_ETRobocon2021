45183bf2f6ffdf2856647667dda6a712927f3970
HirokaneShota <hirokane.shota@gmail.com>
2021-09-07 12:37:00 +0900

    エーフィ変更

--- workspace/src/SlalomEifieInfomation/SlEfCurveInfomation.cpp
+++ workspace/src/SlalomEifieInfomation/SlEfCurveInfomation.cpp
@@ -21,10 +21,12 @@ SlEfCurveInfomation::SlEfCurveInfomation(){
 
     index++;
 */
+/*
     index=1;
     //index=0;
-    curveData[index].radius=48.723;
+    curveData[index].radius=365*0.3527-20;
     curveData[index].dirction=CURVE_RIGHT;
+
 
     index=3;
     //index=2;
@@ -34,6 +36,19 @@ SlEfCurveInfomation::SlEfCurveInfomation(){
     index=4;
     //index=3;
     curveData[index].radius=233*0.3527;
+    curveData[index].dirction=CURVE_LEFT;
+*/
+    //4
+    index=0;//3;
+    curveData[index].radius=100;//117;//48.723;//350*0.3527;
+    curveData[index].dirction=CURVE_RIGHT;
+    //5
+    index++;
+    curveData[index].radius=220;//158.25;//180;//256;
+    curveData[index].dirction=CURVE_RIGHT;
+
+    index++;
+    curveData[index].radius=140;//;//110;//230;
     curveData[index].dirction=CURVE_LEFT;
 
 
@@ -49,4 +64,5 @@ int8 SlEfCurveInfomation::getter(int16 scene_num,CurveData* curve_data){
 
     return SYS_OK;
 }
+
 
