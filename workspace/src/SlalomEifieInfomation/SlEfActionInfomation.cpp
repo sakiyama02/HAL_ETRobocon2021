1840ac88ad1f310d6f06a313e119a5c41d6ea960
HirokaneShota <hirokane.shota@gmail.com>
2021-09-07 19:39:42 +0900

    バク修正

--- workspace/src/SlalomEifieInfomation/SlEfActionInfomation.cpp
+++ workspace/src/SlalomEifieInfomation/SlEfActionInfomation.cpp
@@ -249,8 +249,8 @@ SlEfActionInfomation::SlEfActionInfomation(){
 
     index++;
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //11直進する
-    /*
+    //8直進する
+    
     changeInfoData[index].pos_info_data.potision.yPosition=3900*2*0.3527;
     //changeInfoData[index].pos_info_data.potision.yPosition=3291;//3096
     changeInfoData[index].pos_info_data.xCondition=NONE;
@@ -261,9 +261,9 @@ SlEfActionInfomation::SlEfActionInfomation(){
     changeInfoData[index].section_act=STRAIGHT;
     changeInfoData[index].speed=10;
 
-    index++;*/
+    index++;
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //8旋回する
+    //9旋回する
     changeInfoData[index].direction_data.direction=89;
     changeInfoData[index].direction_data.condition=LOW;
 
@@ -275,7 +275,7 @@ SlEfActionInfomation::SlEfActionInfomation(){
     index++; 
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //9ラインに向かって直進する
+    //10ラインに向かって直進する
     changeInfoData[index].vData.v=65;
     //changeInfoData[index].rgb_data.condition=LOW;
     changeInfoData[index].vData.condition=LOW; 
@@ -285,7 +285,7 @@ SlEfActionInfomation::SlEfActionInfomation(){
 
     index++;
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //10直進する
+    //11直進する
     
     changeInfoData[index].pos_info_data.potision.xPosition=330*2*0.3527;
     //changeInfoData[index].pos_info_data.potision.yPosition=3291;//3096
@@ -299,7 +299,7 @@ SlEfActionInfomation::SlEfActionInfomation(){
     index++;
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //11旋回する
+    //12旋回する
     
     changeInfoData[index].direction_data.direction=165;
     changeInfoData[index].direction_data.condition=HIGH;
@@ -313,7 +313,7 @@ SlEfActionInfomation::SlEfActionInfomation(){
     
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    //12ライントレースする
+    //13ライントレースする
     changeInfoData[index].pos_info_data.potision.yPosition=2680;//2876
     changeInfoData[index].pos_info_data.xCondition=NONE;
     changeInfoData[index].pos_info_data.yCondition=LOW;
@@ -324,7 +324,7 @@ SlEfActionInfomation::SlEfActionInfomation(){
     changeInfoData[index].speed=10;
 
     index++;
-    //13ライントレースする
+    //14ライントレースする
     changeInfoData[index].pos_info_data.potision.xPosition=200;
     changeInfoData[index].pos_info_data.potision.yPosition=2600;//2876
     changeInfoData[index].pos_info_data.xCondition=NONE;
@@ -337,7 +337,7 @@ SlEfActionInfomation::SlEfActionInfomation(){
     index++;
 
 
-    //14ライントレースする
+    //15ライントレースする
     changeInfoData[index].sData.s=80;
     changeInfoData[index].sData.condition=HIGH;
     changeInfoData[index].distance=0;
