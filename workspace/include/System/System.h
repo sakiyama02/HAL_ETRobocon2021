8d9075e093d15dc15ebb087f6aad70e891916cde
HirokaneShota <hirokane.shota@gmail.com>
2021-09-07 12:40:18 +0900

    シーン数変更

--- workspace/include/System/System.h
+++ workspace/include/System/System.h
@@ -11,7 +11,7 @@
 #define SYS_PARAM 2 //引数エラー
 #define TIMEATTACK_NUM 20 
 #define SLALOMEEBUI_NUM 19
-#define SLALOMEIFIE_NUM 20
+#define SLALOMEIFIE_NUM 14
 #define SLALOMBLACKY_NUM 16
 #define GARAGE_NUM 25
 
