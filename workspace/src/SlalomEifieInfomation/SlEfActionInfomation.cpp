bbbd2355649615c2bf48554c559b4b6fc9ccf2c0
HirokaneShota <hirokane.shota@gmail.com>
2021-09-08 15:55:09 +0900

    エーフィ回転変更

--- workspace/src/SlalomEifieInfomation/SlEfActionInfomation.cpp
+++ workspace/src/SlalomEifieInfomation/SlEfActionInfomation.cpp
@@ -238,7 +238,7 @@ SlEfActionInfomation::SlEfActionInfomation(){
 
 
     //7直進する
-    changeInfoData[index].pos_info_data.potision.yPosition=2800;
+    changeInfoData[index].pos_info_data.potision.yPosition=3900*2*0.3527;//2800;
     changeInfoData[index].pos_info_data.xCondition=NONE;
     changeInfoData[index].pos_info_data.yCondition=LOW;
 
